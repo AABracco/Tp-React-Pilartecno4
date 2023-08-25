d0b19a32a05ad0bd85c2eb932da613fd8aba7b06
Agustin <agustinbracco174@gmail.com>
2023-08-25 15:03:00 -0300

    Se finalizo Tp1

---
+++ src/constants/menu.js
@@ -0,0 +1,25 @@
+export const drawerMenu = [
+    {
+        title: "inicio",
+        path: "/",
+    },
+    {
+        title: "pages",
+        children: [
+            {
+                title: "todo",
+                path: "/todo"
+            },
+            {
+                title: "fetchList",
+                path: "/fetch"
+            }
+        ]
+    }
+]
+export const popMenu = [
+    {
+        title: "inicio",
+        path: "/",
+    },
+]
